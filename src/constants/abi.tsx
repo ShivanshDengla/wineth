6223590421a510ff6acef78403586310ee719288
Shivansh Dengla <66008449+ShivanshDengla@users.noreply.github.com>
2024-08-29 11:49:32 +0530

    ABI

--- src/constants/abi.tsx
+++ src/constants/abi.tsx
@@ -1,9 +1,12 @@
 import { Abi } from 'viem';
 import PRIZEPOOL_ABI from './abis/prizepool.json';
+import USDCVAULT_ABI from './abis/usdcvault.json';
+import DEPOISTTOKEN_ABI from './abis/deposittoken.json';
+import WETHPRIZE_ABI from './abis/wethprize.json';
 
 export const ABI = {
   PRIZEPOOL: PRIZEPOOL_ABI as Abi,
-  WETHPRIZE: PRIZEPOOL_ABI as Abi,
-  USDCVAULT: PRIZEPOOL_ABI as Abi,
-  DEPOSITTOKEN: PRIZEPOOL_ABI as Abi,
+  WETHPRIZE: USDCVAULT_ABI as Abi,
+  USDCVAULT: DEPOISTTOKEN_ABI as Abi,
+  DEPOSITTOKEN: WETHPRIZE_ABI as Abi,
 };
