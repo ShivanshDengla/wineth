491049ab9e199214176afdbb8bf935edb46e6317
underethsea <underethsea@gmail.com>
2024-09-23 12:03:02 -0700

    simplify tvl parse

--- src/components/Tvl.tsx
+++ src/components/Tvl.tsx
@@ -41,7 +41,8 @@ const Tvl = () => {
     <div className="fixed bottom-5 left-5 text-white p-3 rounded-lg text-lg z-10">
       <p>
         TVL ${""}
-        {tvl ? ParseVaultAmount(BigInt(tvl.toString()), true) : "N/A"}
+        {tvl ? ParseVaultAmount(tvl, true) : "N/A"}
+
       </p>
       {/* <p>Ethereum Price: {ethPrice ? `$${ethPrice.toFixed(2)}` : 'N/A'}</p> */}
     </div>
