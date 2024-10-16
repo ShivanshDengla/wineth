1d3b306e8280ef260d3c62815f8579c21a058523
underethsea <underethsea@gmail.com>
2024-10-16 06:33:32 -0700

    temp rewards

--- src/constants/rewards.tsx
+++ src/constants/rewards.tsx
@@ -1,11 +1,11 @@
 export const REWARDS = [
-    // {
-    //   GECKO: "optimism",
-    //   PROMOTION: 8, 
-    //   SYMBOL: "OP",
-    //   IMAGE: "https://assets.coingecko.com/coins/images/25244/standard/Optimism.png?1696524385",
-    //   DECIMALS: 1e18,
-    // },
+    {
+      GECKO: "optimism",
+      PROMOTION: 8, 
+      SYMBOL: "OP",
+      IMAGE: "https://assets.coingecko.com/coins/images/25244/standard/Optimism.png?1696524385",
+      DECIMALS: 1e18,
+    },
     // {
     //   GECKO: "pooltogether",
     //   PROMOTION: 12, 
@@ -13,5 +13,6 @@ export const REWARDS = [
     //   IMAGE: "https://assets.coingecko.com/coins/images/14003/standard/PoolTogether.png?1696513732",
     //   DECIMALS: 18, 
     // },
+    
   ];
   
