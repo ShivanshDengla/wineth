10640f20dd7596490cb8d0dc4233134e0de06c49
Shivansh Dengla <shivanshdengla@gmail.com>
2025-03-23 12:54:32 +0530

    more debugg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,10 +40,10 @@ const Home: NextPage = () => {
 
         {/* Farcaster Frame Meta Tags */}
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="https://winwin-git-frames-shivansh-denglas-projects.vercel.app/images/embed.png" />
+        <meta property="fc:frame:image" content="/images/embed.png" />
         <meta property="fc:frame:button:1" content="Visit winEth" />
         <meta property="fc:frame:button:1:action" content="post_redirect" />
-        <meta property="fc:frame:post_url" content="https://winwin-git-frames-shivansh-denglas-projects.vercel.app/api/frame" />
+        <meta property="fc:frame:post_url" content="api/frame" />
 
       </Head>
 
